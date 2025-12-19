59b73a4fc9e6875a942738a90787b4d2a5599291
axizkhan <axizkhan786110@gmail.com>
2025-12-19 16:35:39 +0530

    use get put post patch dele

--- basicApp.js
+++ basicApp.js
@@ -1,7 +1,4 @@
-import express, { json } from 'express'
-
-
-// import { console } from 'node:inspector';
+import express from 'express'
 
 let uid=0;
 
@@ -11,51 +8,85 @@ const app=express();
 app.use(express.json());
 const PORT=5050;
 
-class User{
-     users={
+class Anime{
+     animes={
     
     };
     length=0;
 
     getAllUser(){
-        return this.length && this.users;
+        return this.length && this.animes;
     }
 
     getUser(id){
-        const user =this.users[`${id}`];
+        const user =this.animes[`${id}`];
         return user&&user;
     }
     
-    putUser(newUser,id){
-
-        const user=this.users[`${id}`];
-        const result=user&&(this.users[`${id}`]=newUser)&&true;
+    putUser(body,id){
+         const{name="",mangaWriter="",studio="",rating=0}=body||{};
+         let newAnime={
+            name:name,
+            mangaWriter:mangaWriter,
+            studio:studio,
+            rating:rating,
+        }
+        const Isanime=this.animes[`${id}`];
+        const result=Isanime&&(this.animes[`${id}`]=newAnime)&&true;
         return result;
         
     }
 
-    postUser(user){
-        this.users[`${uid}`]=user;
+    postUser(body){
+        const{name="",mangaWriter="",studio="",rating=0}=body||{};
+
+       
+
+        let newAnime={
+            name:name,
+            mangaWriter:mangaWriter,
+            studio:studio,
+            rating:rating,
+        }
+        this.animes[`${uid}`]=newAnime;
         uid++;
         this.length++;
         return true;
     }
 
     deleteUser(id){
-       const user= this.users[`${id}`];
-       const result=user &&(delete this.users[`${id}`])&&true;
+       const anime= this.animes[`${id}`];
+       const result=anime &&(delete this.animes[`${id}`])&&true;
        this.length--;
        return result;
+    }
+
+    patchUser(body,id){
+        let anime=this.animes[`${id}`];
+
+        if(anime){
+            for(let key in body){
+                anime[key]=body[key];
+            }
+            return true;
+        }
+
+        return false;
+
+
+      
+        
+
     }
 
 
 }
 
-const users=new User();
+const anime=new Anime();
 
 
 app.get("/",(req,res)=>{
-    const allUser=users.getAllUser();
+    const allUser=anime.getAllUser();
     if(allUser){
        return res.send(allUser);
     }
@@ -65,30 +96,175 @@ app.get("/",(req,res)=>{
 app.get("/:id",(req,res)=>{
     
     const {id}=req.params;
-    const user=users.getUser(id);
-    console.log(user);
+    const user=anime.getUser(id);
     res.json(user);
 })
 
 app.post("/",(req,res)=>{
-    const result=users.postUser(req.body);
+
+    
+    const result=anime.postUser(req.body);
+    
     res.send(result);
 })
 
 app.put("/:id",(req,res)=>{
 
     const {id}=req.params;
-    const result=users.putUser(req.body,id);
+    const result=anime.putUser(req.body,id);
+    res.send(result);
+})
+
+app.patch("/:id",(req,res)=>{
+    const {id}=req.params;
+    const result=anime.patchUser(req.body,id);
     res.send(result);
 })
 
 app.delete("/:id",(req,res)=>{
     const {id}=req.params;
-    const result=users.deleteUser(id);
+    const result=anime.deleteUser(id);
+    // console.log(req.body);
     res.send(result);
     
 })
 
 app.listen(PORT,()=>{
     console.log(`Server is listening ${PORT}`);
+
 });
+   
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app = express();
+// app.use(express.json());
+
+// const PORT = 5050;
+
+// /* =========================
+//    Global State
+// ========================= */
+// let uid = 0;
+// let length = 0;
+
+// const users = {};
+
+// /* =========================
+//    Global Functions
+// ========================= */
+
+// function getAllUsers() {
+//   if (length > 0) {
+//     return users;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function getUser(id) {
+//   if (users[id]) {
+//     return users[id];
+//   } else {
+//     return false;
+//   }
+// }
+
+// function postUser(user) {
+//   users[uid] = user;
+//   uid++;
+//   length++;
+//   return true;
+// }
+
+// function putUser(id, newUser) {
+//   if (users[id]) {
+//     users[id] = newUser;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function deleteUser(id) {
+//   if (users[id]) {
+//     delete users[id];
+//     length--;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// /* =========================
+//    Routes
+// ========================= */
+
+// app.get("/", (req, res) => {
+//   const allUsers = getAllUsers();
+
+//   if (allUsers) {
+//     res.send(allUsers);
+//   } else if (allUsers === false) {
+//     res.send(false);
+//   } else {
+//     res.send(false);
+//   }
+// });
+
+// app.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const user = getUser(id);
+
+//   if (user) {
+//     res.json(user);
+//   } else {
+//     res.json(false);
+//   }
+// });
+
+// app.post("/", (req, res) => {
+//   const result = postUser(req.body);
+
+//   if (result) {
+//     res.send(true);
+//   } else {
+//     res.send(false);
+//   }
+// });
+
+// app.put("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const result = putUser(id, req.body);
+
+//   if (result) {
+//     res.send(true);
+//   } else {
+//     res.send(false);
+//   }
+// });
+
+// app.delete("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const result = deleteUser(id);
+
+//   if (result) {
+//     res.send(true);
+//   } else {
+//     res.send(false);
+//   }
+// });
+
+// /* =========================
+//    Server
+// ========================= */
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on ${PORT}`);
+// });
